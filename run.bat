93c854ac1cab5843bb59c509b739c86267c5ab35
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-03-08 14:16:38 +0300

    made changes to the projecrt

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\ASUS\OneDrive\Desktop\Daily-Git-Commit-main\Daily-Git-Commit-main" & "C:\Users\ASUS\AppData\Local\Programs\Python\Python310\python.exe" "c:/Users/ASUS/OneDrive/Desktop/Daily-Git-Commit-main/Daily-Git-Commit-main/main.py"
+
