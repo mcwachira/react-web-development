df12ce0cea5a613b6c5362d654dd99d5f29a36f3
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-03-08 14:21:19 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\ASUS\OneDrive\Desktop\Daily-Git-Commit-main\Daily-Git-Commit-main" & "C:\Users\ASUS\AppData\Local\Programs\Python\Python310\python.exe" "c:/Users/ASUS/OneDrive/Desktop/Daily-Git-Commit-main/Daily-Git-Commit-main/main.py"
-
